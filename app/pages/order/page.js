590d7f85a21d8a1b48fa15aa336edf6102d7ef06
zpintern <interns.tech@myzeepay.com>
2023-10-10 08:49:06 +0000

    structuring project architecture to adhere to app router

---
+++ app/pages/order/page.js
@@ -0,0 +1,7 @@
+export default function OrderPage(){
+    return (
+        <main className="text-4xl bg-white h-screen">
+            This is the order page
+        </main>
+    )
+}
---
+++ app/pages/our_story/page.js
@@ -0,0 +1,5 @@
+export default function OurStoryPage(){
+    <main className="text-4xl">
+        This is the Our Story Page
+    </main>
+}
