41e5ac08d33e9d5b6d6a87251a8b8a5a8d61c36f
zpintern <interns.tech@myzeepay.com>
2023-10-14 14:58:19 +0000

    configured mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./database/connect");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.BACKEND_PORT || 3001;
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello from the backend!' });
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello from the backend!" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+async function run(){
+  try {
+    await connectDB(process.env.MONGOOSE_URI || 3001);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-console.log(process.env.BACKEND_PORT)
+run()
