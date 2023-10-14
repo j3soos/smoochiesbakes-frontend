6f3e327d941c73dedfbf4e4fb109c60938acd9bb
zpintern <interns.tech@myzeepay.com>
2023-10-14 21:47:16 +0000

    deleted old models folder

--- app/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: Number, required: false },
-});
-
-export default mongoose.model('User', UserSchema);
