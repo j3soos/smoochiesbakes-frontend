e57f490d78ff56f345843cf65254bbe236169370
zpintern <interns.tech@myzeepay.com>
2023-10-15 20:01:14 +0000

    updated category in  product model

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true },
+  category: {
+    type: String,
+    enum: {
+      values: ["Full Cakes", "Cupcakes", "Banacakes"],
+      message: "Invalid Category",
+    },
+    required: true,
+  },
   price: { type: Number, required: true },
   description: { type: String, required: false },
 });
 
-module.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model("Product", ProductSchema);
