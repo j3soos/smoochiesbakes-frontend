b9a6230363976942ca62059dcdef86da7fcc9853
j3soos <k.opoku@outlook.com>
2023-10-24 16:46:11 +0000

    fixed image sizes in viewModal

--- app/pages/products/viewProductModal.js
+++ app/pages/products/viewProductModal.js
@@ -3,7 +3,12 @@ import React, { useState } from "react";
 import Modal from "@/app/components/modal";
 
 // const ViewProductModal = ({ isOpen, onClose, selectedRowData }) => {
-const ViewProductModal = ({ isOpen, onClose, selectedProduct, handleAddToCart }) => {
+const ViewProductModal = ({
+  isOpen,
+  onClose,
+  selectedProduct,
+  handleAddToCart,
+}) => {
   const [name, setName] = useState(selectedProduct.name);
   const [price, setPrice] = useState(selectedProduct.price);
   const [description, setDescription] = useState(selectedProduct.description);
@@ -15,18 +20,46 @@ const ViewProductModal = ({ isOpen, onClose, selectedProduct, handleAddToCart })
   }
 
   return (
-    <Modal header="Detailed View" closeModal={()=>{onClose()}}>
-        <div className="flex flex-col h-full w-full rounded">
-            <img src="/order_page/cookies.jpg" className="rounded-lg pb-2"/>
-            <div className="flex flex-row text-black font-semibold text-sm">NAME: {name}</div>
-            <div className="flex flex-row text-black font-semibold text-sm">PRICE: GHS {price}</div>
-            <div className="flex flex-row text-black font-light pb-2">{description}</div>
-            <button onClick={()=>{
-                setAddedToCart(true)
-                handleAddToCart()
-                }} className="btn bg-pink-500 rounded-md w-28 text-white font-bold text-sm py-2 px-3">Add to Cart</button>
-            {addedToCart && <div className="text-xs font-extralight text-red-800">Product Added To Cart</div>}
+    <Modal
+      header="Detailed View"
+      closeModal={() => {
+        onClose();
+      }}
+    >
+      <div className="flex flex-col h-full w-full rounded">
+        <div
+          className="rounded-lg w-full h-96"
+          style={{
+            backgroundImage: `url(${selectedProduct.bg_url})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        />
+        <div className="flex flex-row text-black font-semibold text-sm">
+          NAME: {name}
         </div>
+        <div className="flex flex-row text-black font-semibold text-sm">
+          PRICE: GHS {price}
+        </div>
+        <div className="flex flex-row text-black font-light pb-2">
+          {description}
+        </div>
+        <button
+          onClick={() => {
+            setAddedToCart(true);
+            handleAddToCart();
+          }}
+          className="btn bg-pink-500 rounded-md w-28 text-white font-bold text-sm py-2 px-3"
+        >
+          Add to Cart
+        </button>
+        {addedToCart && (
+          <div className="text-xs font-extralight text-red-800">
+            Product Added To Cart
+          </div>
+        )}
+      </div>
     </Modal>
   );
 };
