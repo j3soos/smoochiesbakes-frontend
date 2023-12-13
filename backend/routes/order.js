042d763c80752f076a7a35c5388a74ab9a35c246
kkopoku <kk.opoku@outlook.com>
2023-12-13 09:40:31 +0000

    seperated frontend from backend

--- backend/routes/order.js
+++
@@ -1,7 +0,0 @@
-const {makeOrder, confirmOrderPayment} = require('../controller/order');
-const router = require('express').Router();
-
-router.post('/makeOrder', makeOrder)
-router.post('/confirmOrderPayment/:order_id', confirmOrderPayment)
-
-module.exports = router;
--- backend/routes/product.js
+++
@@ -1,7 +0,0 @@
-const {createProduct, getCategoryProducts} = require('../controller/product');
-const router = require('express').Router();
-
-router.post('/createProduct', createProduct)
-router.get('/getCategoryProducts', getCategoryProducts)
-
-module.exports = router;
